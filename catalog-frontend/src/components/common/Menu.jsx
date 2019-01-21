b1bc5319752d01b770792cc8f3f1e899e90ac10d
Mohammad Alhomaid <mohammad.a.7@outlook.com>
2019-01-21 22:48:29 +0300

    fixed card onClick Link

--- catalog-frontend/src/components/common/Menu.jsx
+++ catalog-frontend/src/components/common/Menu.jsx
@@ -2,30 +2,34 @@ import React from 'react'
 import { Card, Elevation, Divider, Icon, Text } from '@blueprintjs/core'
 import { IconNames } from "@blueprintjs/icons"
 import { Rating, Price } from './';
+import { Link } from 'react-router-dom'
+
 
 function Menu(props) {
   return (
     <div className="App-Main-Container">
-        {props.content.map((item) => {
-          return <Card key={item.id} className="Main-Menu-Card" onClick={props.handleClick} interactive={true} elevation={Elevation.TWO}>
+      {props.content.map((item) => {
+        return <Link to={`/${item.category_id}/${item.id}`}>
+          <Card key={item.id} className="Main-Menu-Card" onClick={props.handleClick} interactive={true} elevation={Elevation.TWO}>
             <div className="Main-Menu-Card-Image-Container">
               <img className="Main-Menu-Card-Image" src={item.image_path} alt={item.name} />
             </div>
             <Divider />
             <Text className="capitalize left-align title">{item.name}</Text>
             <Text ellipsize={true} className="capitalize left-align subtitle">{item.description}</Text>
-            
+
             <div className="item-price-rating-container">
               <Rating rating={item.rating} />
               <Price price={item.price} />
             </div>
             <div className="item-user-container">
-              <Icon style={{ color: '#999' }} icon={IconNames.USER}/>
+              <Icon style={{ color: '#999' }} icon={IconNames.USER} />
               <p className="item-user-name">{item.user_name}</p>
             </div>
           </Card>
-        })}
-      </div>
+        </Link>
+      })}
+    </div>
   )
 }
 
