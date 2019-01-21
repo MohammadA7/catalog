bd04fddd44e7a13591b7277c646dfaac22cecd96
Mohammad Alhomaid <mohammad.a.7@outlook.com>
2019-01-21 22:22:48 +0300

    added react-router

--- catalog-frontend/src/App.js
+++ catalog-frontend/src/App.js
@@ -1,46 +1,18 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { NavigationBar, Content, SideMenu, Footer } from './components'
-import axios from 'axios'
+import { Route, Switch } from "react-router-dom";
+import { Home, Register, Login } from './components';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      content: [],
-      selectedCatagory: ''
-    }
-    this.handleCatagoryChange = this.handleCatagoryChange.bind(this)
-  }
-  
-  componentDidMount() {
-    axios.get(`http://${process.env.REACT_APP_BACK_END_IP}/`)
-      .then((response => {
-        this.setState({
-          content: response.data,
-        })
-      }))
-      .catch(error => console.log(error))
-  }
-
-  handleCatagoryChange(id) {
-    this.setState({ selectedCatagory: id })
-  }
-
-  render() {
+ render() {
     return (
-      <div className="App">
-        <NavigationBar className="navbar" />
-        <SideMenu className="App-Side-Menu"
-          content={this.state.content}
-          selectedCatagory={this.state.selectedCatagory}
-          onSelectedCatagoryChange={this.handleCatagoryChange} />
-        <div className="App-Content">
-          <Content className="App-Main-Container" 
-            selectedCatagory={this.state.selectedCatagory} />  
-          <Footer className="App-Footer" />
-        </div>
-      </div>
+      <Switch>
+        <Route path={"/:catagory?/:item?"} render={({ match }) => {
+        return <Home match={match} className="App-Main-Container" />
+      }} />
+      <Route exact path={"/login"} component={Login}/>
+      <Route exact path={"/register"} component={Register}/>
+      </Switch>
     );
   }
 }
