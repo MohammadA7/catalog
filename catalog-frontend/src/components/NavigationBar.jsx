625442b3fb678e69898638672a6959412f773e4f
Mohammad Alhomaid <mohammad.a.7@outlook.com>
2019-01-23 21:40:59 +0300

    added navigation bar links

--- catalog-frontend/src/components/NavigationBar.jsx
+++ catalog-frontend/src/components/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Navbar, Button, Alignment } from '@blueprintjs/core';
+import { Link } from 'react-router-dom'
 
 class NavigationBar extends Component {
   render() {
@@ -9,8 +10,27 @@ class NavigationBar extends Component {
           <Navbar.Group align={Alignment.LEFT}>
             <Navbar.Heading>Catalog</Navbar.Heading>
             <Navbar.Divider />
-            <Button className="bp3-minimal" icon="home" text="Home" />
-            <Button className="bp3-minimal" icon="document" text="Files" />
+            <Link to="/">
+              <Button className="bp3-minimal" icon="home" text="Home" />
+            </Link>
+          </Navbar.Group>
+
+          <Navbar.Group align={Alignment.RIGHT}>
+            {
+              this.props.isLoggedIn ?
+                <Link to="/logout">
+                  <Button className="bp3-minimal" icon="log-out" text="Logout" />
+                </Link>
+                :
+                <div>
+                  <Link to="/login">
+                  <Button className="bp3-minimal" icon="log-in" text="Login" />
+                </Link>
+                <Link to="/register">
+                  <Button className="bp3-minimal" icon="new-person" text="Register" />
+                </Link>
+                </div>
+            }
           </Navbar.Group>
         </Navbar>
       </div>
