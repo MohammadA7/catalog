535a9c62f04bed5a1fe1b2db7e40d74d7ba510a5
Mohammad Alhomaid <mohammad.a.7@outlook.com>
2019-01-27 09:15:10 +0300

    added login functionality,

--- catalog-frontend/src/App.jsx
+++ catalog-frontend/src/App.jsx
@@ -1,29 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import './App.scss';
-import { Route, Switch } from "react-router-dom";
-import { Home, Register, Login, NavigationBar } from './components';
+import { Route, Switch } from "react-router-dom"
+import { Home, Register, Login, NavigationBar } from './components'
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: false,
+      token: '',
     }
+    this.onLoginSuccess = this.onLoginSuccess.bind(this)
   }
+
+  onLoginSuccess(token) {
+    this.setState({
+      isLoggedIn: true,
+      token: token
+    })
+  }
+  onLogout() {
+    axios.get()
+    this.setState({
+      isLoggedIn: false,
+      token: ''
+    })
+  }
+
   render() {
     return (
       <div>
         <NavigationBar isLoggedIn={this.state.isLoggedIn} />
         <Switch>
-          <Route exact path={"/login"} isLoggedIn={this.state.isLoggedIn} component={Login} />
-          <Route exact path={"/register"} isLoggedIn={this.state.isLoggedIn} component={Register} />
+          <Route exact path={"/login"} render={() => <Login
+            isLoggedIn={this.state.isLoggedIn}
+            onLoginSuccess={this.onLoginSuccess} />} />
+          <Route exact path={"/logout"} render={() => this.onLogout()} />
+          <Route exact path={"/register"} render={() => <Register
+            isLoggedIn={this.state.isLoggedIn} />} />
           <Route path={"/:catagory?/:item?"} render={({ match }) => {
             return <Home isLoggedIn={this.state.isLoggedIn} match={match} className="App-Main-Container" />
           }} />
         </Switch>
       </div>
-
-    );
+    )
   }
 }
 
--- catalog-frontend/src/components/Login.jsx
+++ catalog-frontend/src/components/Login.jsx
@@ -1,12 +1,102 @@
 import React, { Component } from 'react'
+import {
+  Card, FormGroup, InputGroup,
+  H3, Button, Text, H5
+} from '@blueprintjs/core'
+import axios from "axios";
+import { AppToaster } from './toster'
+import { Redirect } from 'react-router-dom'
 
 class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.onRegularLoginSubmit = this.onRegularLoginSubmit.bind(this)
+    this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    this.handlePasswordChange = this.handlePasswordChange.bind(this)
+  }
+
+  onRegularLoginSubmit() {
+    axios.get('/token', {
+      method: 'get',
+      baseURL: `http://${process.env.REACT_APP_BACK_END_IP}/`,
+      auth: {
+        username: this.state.username,
+        password: this.state.password
+      }
+    })
+      .then((response) => {
+        this.props.onLoginSuccess(response.data.token)
+        AppToaster.show({
+          message: 'Logged in successfully',
+          intent: 'success',
+          icon: 'tick'
+        })
+      })
+      .catch(error => {
+        console.log(error)
+        if (error.response) {
+          AppToaster.show({
+            message: error.response.data,
+            intent: 'danger',
+            icon: 'error'
+          })
+        }
+      })
+  }
+
+
+  handleUsernameChange(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
   render() {
-    return (
-      <div>
-        Login
-      </div>
-    )
+    if (this.props.isLoggedIn)
+      return <Redirect to='/' />
+    else
+      return (
+        <div className="Login-Container">
+          <Card style={{ margin: '3%', padding: '1.5%' }}>
+            <H3>Login</H3>
+            <H5>Login with google account</H5>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <div style={{ width: '42%', height: '1px', background: '#ccc' }} />
+              <Text>OR</Text>
+              <div style={{ width: '42%', height: '1px', background: '#ccc' }} />
+            </div>
+            <H5>Login with regular account</H5>
+            <FormGroup
+              label="Email"
+              labelFor="email"
+              labelInfo="(required)"
+            >
+              <InputGroup id="email" placeholder="example@gmail.com" leftIcon="user" onChange={this.handleUsernameChange} />
+            </FormGroup>
+            <FormGroup
+              label="Password"
+              labelFor="password"
+              labelInfo="(required)"
+            >
+              <InputGroup id="password" placeholder="Password" type="password" leftIcon="lock" onChange={this.handlePasswordChange} />
+            </FormGroup>
+            <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Button text="Cancel" style={{ margin: '1%' }} />
+              <Button text="Login" onClick={this.onRegularLoginSubmit} intent="primary" style={{ margin: '1%' }} />
+            </div>
+          </Card>
+        </div>
+      )
   }
 }
 
